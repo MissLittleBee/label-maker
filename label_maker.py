c9383fac2d037ede8f60be490a309c2bf99df771
Little Bee <crazy-b@seznam.cz>
2022-11-01 18:45:19 +0100

    Data import can be chosen from user or csv

--- label_maker.py
+++ label_maker.py
@@ -5,6 +5,7 @@ import logging
 
 from calc import calculate_unit_price
 from inputs import csv_input
+from inputs import user_input
 from outputs import to_word
 
 log = logging.getLogger(__name__)
@@ -13,9 +14,21 @@ log = logging.getLogger(__name__)
 def main():
     log.info(' program start '.center(80, '-'))
     # TODO: vyměnit za argument od argparse
-    file_path = "input/sample_data.csv"
 
-    data = csv_input(file_path)
+    data = []
+    data_input = "file"
+
+    if data_input == "file":
+        file_path = "input/sample_data.csv"
+        data = csv_input(file_path)
+
+    elif data_input == "user":
+        data = user_input()
+
+    else:
+        log.warning(f"Neznámá možnost vstupu: {data_input}")
+        exit()
+
     calculated_data = calculate_unit_price(data)
     to_word(calculated_data, 'templates/labels_template.docx')
     log.info(' program end '.center(80, '-'))
